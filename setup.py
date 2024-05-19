c75a7d30dfebc30960cd2e2f4683541f9db4f70d
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-19 08:21:12 +0200

    Added description and github page

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='llm_parametrizer',
-    version='0.1',
+    version='0.1.1',
     author ="Alejandro Camus",
     description="A library to parametrize multiple API calls for LLMs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=['llm_parametrizer'],
+    url="https://github.com/arjuna-dev/llm_parametrizer",
     install_requires=[
         'openai',
-        'python-dotenv',
+        'python-dotenv'
     ],
 )
